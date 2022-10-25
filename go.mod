f4cf05dc49ebb54b495a3ceffabfb0750838edc8
bayu <bayu@ipaymu.com>
2022-10-25 14:03:50 +0800

    robbitmq

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.6.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
 	github.com/ugorji/go v1.2.5 // indirect
 	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf
 	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
--- main.go
+++ main.go
@@ -11,7 +11,15 @@ import (
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"github.com/streadway/amqp"
 )
+
+func errorWrapper(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,6 +28,17 @@ func main() {
 	}
 
 	db, err := gorm.Open(postgres.Open(Config.DbURL(Config.BuildDBConfig())), &gorm.Config{})
+
+	//----------------rabitmq-----------------
+	conn, errs := amqp.Dial(os.Getenv("RABBITMQ_HOST"))
+	errorWrapper(errs, "Failed to connect rabbitmq")
+	defer conn.Close()
+
+	ch, errs := conn.Channel()
+	errorWrapper(errs, "Failed to open a channel")
+	defer ch.Close()
+
+	//---------------------------------------------
 
 	if err != nil {
 		fmt.Println("Status DB:", err)
@@ -35,6 +54,7 @@ func main() {
 		&Models.CategoryRelation{},
 		&Models.Setting{})
 
+	Routes.SubscribeMessage(ch, "go-queue_order")
 	router := Routes.SetupRouter(db)
 	router.Run(":" + os.Getenv("APP_PORT"))
 
