208ec9d9ce8b6cb1cc5ea35018ed5e414e13e9db
bayu <bayu@ipaymu.com>
2023-01-18 14:58:05 +0800

    coupon by branch

--- handler/coupon.go
+++ handler/coupon.go
@@ -36,6 +36,7 @@ func (h *couponHandler) CreateCoupon(c *gin.Context) {
 	_, err = h.couponService.CreateService(input, c.MustGet("currentUser").(user.User).ID)
 
 	if err != nil {
+
 		response := helper.APIResponse("Create coupon failed", http.StatusBadRequest, "error", err)
 		c.JSON(http.StatusBadRequest, response)
 		return
@@ -62,6 +63,7 @@ func (h *couponHandler) UpdateCoupon(c *gin.Context) {
 	_, err = h.couponService.UpdateService(input)
 
 	if err != nil {
+
 		response := helper.APIResponse("Update coupon failed", http.StatusBadRequest, "error", err)
 		c.JSON(http.StatusBadRequest, response)
 		return
@@ -77,6 +79,32 @@ func (h *couponHandler) FindAllCoupon(c *gin.Context) {
 
 	if err != nil {
 		response := helper.APIResponse("Search All Branch failed", http.StatusBadRequest, "error", err)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := coupon.FormatCoupons(items)
+	response := helper.APIResponse("Coupon detail", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *couponHandler) FindAllCouponByBranch(c *gin.Context) {
+
+	var input coupon.SearchInput
+	errs := c.ShouldBindUri(&input)
+
+	if errs != nil {
+		errors := helper.FormatValidationError(errs)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("URI input Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	items, err := h.couponService.FindAllByBranchService(input)
+
+	if err != nil {
+		response := helper.APIResponse("Search All Coupon failed", http.StatusBadRequest, "error", err)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
